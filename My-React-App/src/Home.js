223fee5879ff96276676a5a7e430c18a2efefb1f
arduino731 <arduino731@gmail.com>
2023-09-12 13:00:26 -0500

    deploy port

--- My-React-App/src/Home.js
+++ My-React-App/src/Home.js
@@ -1,8 +1,13 @@
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
+import file from "./data/db.json";
+
+// const {file2} = file;
+console.log(file.blogs);
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:3001/blogs')
+  // const { error, isPending, data: blogs } = useFetch('http://localhost:3000/blogs')
+  const { error, isPending, data: blogs } = useFetch(file.blogs);
 
   return (
     <div className="home">
