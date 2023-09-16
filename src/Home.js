ac95e49fb97c14a6dd2a0d20d2a9c0ef4c6e49d0
arduino731 <arduino731@gmail.com>
2023-09-16 18:54:34 -0500

    reorganize folders as finalized

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,16 @@ import useFetch from "./useFetch";
 
 const Home = () => {
   // const { error, isPending, data: blogs } = useFetch('localhost:3000/blogs')
-  // const { error, isPending, data: blogs } = useFetch('https://my-json-server.typicode.com/arduino731/React-portfolio/blogs/')
+  const { error, isPending, data: blogs } = useFetch('https://my-json-server.typicode.com/arduino731/React-portfolio/blogs/')
   // const { error, isPending, data: blogs } = useFetch('https://arduino731.github.io/data/db.json')
-  const { error, isPending, data: blogs } = useFetch("/data/db.json");
-  console.log(blogs)
+  // const { error, isPending, data: blogs } = useFetch("/data/db.json");
+  // console.log(blogs)
 
   return (
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs.blogs} /> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
